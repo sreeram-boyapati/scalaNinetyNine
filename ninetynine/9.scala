dbd6a7c6d3ff580548e8431c404148de36fd04b3
Sreeram Boyapati <sreeram.boyapati2011@gmail.com>
2015-04-22 11:17:02 +0530

    smplify 9

--- ninetynine/9.scala
+++ ninetynine/9.scala
@@ -1,30 +1,27 @@
 object Problem9{
 
   def pack(list: List[Any]): List[Any] = { 
-    def recPack(remainingTail: List[Int], smallList: List[Any]): (List[Any], List[Any]) = smallList match{
+    def recPack(smallList: List[Any]): (List[Any], List[Any]) = smallList match{
       case Nil => (Nil, Nil)
       case (head :: tail) => tail match {
         case Nil => (Nil, List[Any](head))
         case x if x(0) == head => {
-          val (i, j) = recPack(Nil, x)
+          val (i, j) = recPack(x)
           return (i, List[Any](head) ::: j)
         }
         case x if x(0) != head => { 
-          return (remainingTail ::: x, List[Any](head))
+          return (x, List[Any](head))
         }
       }
     }
     var answer = List[Any]()
-    var (remainingTail, processedHead) = (List[Any](), List[Any]())
-    remainingTail = list
-    var length: Int = 0
+    var (remainingTail, processedHead) = (list, List[Any]())
     do {
-      val i = recPack(Nil, remainingTail);
+      val i = recPack(remainingTail);
       remainingTail = i._1;
-      length = remainingTail.length
       processedHead = i._2;
       answer = answer ::: List(processedHead)
-    } while(length != 0)
+    } while(remainingTail != Nil)
     return answer
   }
 
